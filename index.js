273b2f15aa3f2213034efc47583c1500c7bbdbfb
alexrod94 <alexrod94@gmail.com>
2023-02-06 20:26:24 +0100

    Create problems

---
+++ index.js
@@ -0,0 +1,20 @@
+// 1. Write a function that takes an array of numbers and returns the largest number in the array.
+function largestNumber(array) {}
+
+// 2. Write a function that takes a string and returns the number of vowels in the string.
+function numberOfVowels(string) {}
+
+// 3. Write a function that takes two arrays of numbers and returns a new array with all the elements from both arrays, without any duplicates.
+function combineArrays(array1, array2) {}
+
+// 4. Write a function that takes an array of numbers and returns a new array with all the odd numbers in the original array.
+function oddNumbers(array) {}
+
+// 5. Write a function that takes an array of numbers and returns an array with all the even numbers in the original array.
+function evenNumbers(array) {}
+
+// 6. Write a function that takes two strings and returns a string that contains the characters that appear in both strings.
+function commonCharacters(string1, string2) {}
+
+// 7. Write a function that takes an object and returns a new object with all the keys and values reversed (BONUS)
+function reverseObject(object) {}
